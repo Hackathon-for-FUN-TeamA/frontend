adce30108f6a6fad428760b249158a457809dbaf
atomic928 <64305950+atomic928@users.noreply.github.com>
2022-11-30 21:45:20 +0900

    Revert "Fix/viewmodel"

--- Hackathon2022/app/src/main/java/com/example/hackathon2022/model/Date.kt
+++ Hackathon2022/app/src/main/java/com/example/hackathon2022/model/Date.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "date_database")
+@Entity(tableName = "date")
 data class Date(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "date") val date: String?
--- Hackathon2022/app/src/main/java/com/example/hackathon2022/ui/map/MapFragment.kt
+++ Hackathon2022/app/src/main/java/com/example/hackathon2022/ui/map/MapFragment.kt
@@ -1,22 +1,18 @@
 package com.example.hackathon2022.ui.map
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.setFragmentResult
-import androidx.fragment.app.setFragmentResultListener
-import com.example.hackathon2022.SensorViewModel
 import com.example.hackathon2022.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
 
     private var _binding: FragmentMapBinding? = null
-    private val viewModel: SensorViewModel by activityViewModels()
+    private val mapViewModel: MapViewModel by activityViewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView
@@ -31,13 +27,8 @@ class MapFragment : Fragment() {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        setFragmentResultListener("stringData") { key, bundle ->
-            val result = bundle.getString("itemString")
-            Log.d("resultTest", result.toString())
-        }
-
         val ivMap: ImageView = binding.ivMap
-        viewModel.map.observe(viewLifecycleOwner) {
+        mapViewModel.map.observe(viewLifecycleOwner) {
             ivMap.setImageBitmap(it)
         }
 
